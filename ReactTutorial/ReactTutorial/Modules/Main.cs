d4d77fd8bc85b402f3f95e7381ad4f58fc6a17f2
justin1i <justin1i@126.com>
2016-11-29 17:11:11 +0800

    server arch

---
+++ ReactTutorial/ReactTutorial/Modules/Main.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReactTutorial.Modules
+{
+    public class Main : NancyModule
+    {
+        public Main()
+        {
+            Get["/"] = args => View["home"];
+        }
+    }
+}
